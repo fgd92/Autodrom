614a24dafb44553e868d9f143da83220716bfcfc
fgd <vinockurow.iwan2015@yande.ru>
2021-03-13 18:47:01 +0300

    Первый коммит

---
+++ Assets/Scripts/Sensor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sensor : MonoBehaviour
+{
+    Vector3 speed;
+    Vector3 old_position;
+    public CarController CarController;
+    public Text ViewSpeed;
+
+    Transform carTransform;
+
+    void Start()
+    {
+        old_position = transform.position;
+        carTransform = CarController.transform;
+    }
+    void FixedUpdate()
+    {
+        speed = ((carTransform.transform.position - old_position) / Time.fixedDeltaTime); 
+        old_position = carTransform.transform.position; 
+        ViewSpeed.text = (speed.magnitude * 60).ToString();
+    }
+}
