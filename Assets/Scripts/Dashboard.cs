c2c254e3aa64e9b47455438f73e2d427b98ee82b
fgd <vinockurow.iwan2015@yande.ru>
2021-03-14 23:52:45 +0300

    Добавлены модели конуса, эстакады. Обновлена модель трактора.

--- Assets/Scripts/Dashboard.cs
+++ Assets/Scripts/Dashboard.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Sensor : MonoBehaviour
+public class Dashboard : MonoBehaviour
 {
     Vector3 speed;
     Vector3 old_position;
