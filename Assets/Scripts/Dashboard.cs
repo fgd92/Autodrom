6eb6b0903502ffbe54bd457242c9cf0803666981
fgd92 <vinockurow.iwan2015@yandex.ru>
2021-03-16 15:35:33 +0300

    Добавлен прицеп

--- Assets/Scripts/Dashboard.cs
+++ Assets/Scripts/Dashboard.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,14 +10,22 @@ public class Dashboard : MonoBehaviour
     Vector3 old_position;
     public Transform ArrowSpeedometer;
     public Transform ArrowTachospeedometer;
+    public Transform ArrowVoltage;
+    public Transform ArrowOilPressure;
+    public Transform ArrowTemprature;
 
     Transform carTransform;
     CarController carController;
 
-    private float minAngle = -43;
-    private float maxAngle = 43;
-    private float maxSpeed = 45;
-
+    private float maxSpeed = 35;
+    #region Speedometer
+    private float minAngleSpeed = -43;
+    private float maxAngleSpeed = 43;
+    #endregion
+    #region Tachospeedometer
+    private float minAngleTachoSpeed = -130;
+    private float maxAngleTachoSpeed = 45;
+    #endregion
 
     void Start()
     {
@@ -24,15 +33,37 @@ public class Dashboard : MonoBehaviour
         carController = FindObjectOfType<CarController>();
         carTransform = carController.transform;
     }
-    void FixedUpdate()
-    {
-        ShowSpeed();
-    }
-
-    private void ShowSpeed()
+    private void CalculateSpeed()
     {
         speed = ((carTransform.transform.position - old_position) / Time.fixedDeltaTime);
         old_position = carTransform.transform.position;
-        ArrowSpeedometer.localRotation = Quaternion.AngleAxis(Mathf.LerpAngle(minAngle, maxAngle, speed.magnitude / maxSpeed), Vector3.up);
+    }
+    void FixedUpdate()
+    {
+        CalculateSpeed();
+        ShowSpeed();
+        ShowTachospeed();
+        ShowTemprature();
+        ShowOilPressure();
+    }
+
+    private void ShowTemprature()
+    {
+        ArrowTemprature.localRotation = Quaternion.AngleAxis(Mathf.Lerp(minAngleSpeed, maxAngleSpeed, 25f / 40f), Vector3.up);
+    }
+
+    private void ShowOilPressure()
+    {
+        ArrowOilPressure.localRotation = Quaternion.AngleAxis(Mathf.Lerp(1,1,2), Vector3.up);
+    }
+
+
+    private void ShowSpeed()
+    {
+        ArrowSpeedometer.localRotation = Quaternion.AngleAxis(Mathf.Lerp(minAngleSpeed, maxAngleSpeed, speed.magnitude / maxSpeed), Vector3.up);
+    }
+    private void ShowTachospeed()
+    {
+        ArrowTachospeedometer.localRotation = Quaternion.AngleAxis(Mathf.Lerp(minAngleTachoSpeed, maxAngleTachoSpeed, speed.magnitude / maxSpeed), Vector3.up);
     }
 }
---
+++ Assets/Scripts/Reductor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Steps
+{
+    First,
+    Second,
+    Third,
+    Fouth,
+    Fifth,
+    Sixth,
+    Seventh,
+    Eighth,
+    Ninth,
+    Tenth
+}
+
+public class Reductor : MonoBehaviour
+{
+    public Steps CurrentSteps;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
