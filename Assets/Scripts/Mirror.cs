5c46a36934dd081c3d3ac830ba16707028665b55
fgd92 <vinockurow.iwan2015@yandex.ru>
2021-03-26 13:55:58 +0300

    Переимпортированы текстуры, обновлены материлы

---
+++ Assets/Scripts/Mirror.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+
+[RequireComponent(typeof(PlanarReflectionProbe))]
+public class Mirror : MonoBehaviour
+{
+    private PlanarReflectionProbe planarReflectionProbe;
+
+    void Start()
+    {
+        planarReflectionProbe = GetComponent<PlanarReflectionProbe>();
+    }
+    private void Update()
+    {
+        
+    }
+    //Работает даже при просмотре на объект из viewport'а
+    private void OnBecameInvisible()
+    {
+        planarReflectionProbe.enabled = false;
+    }
+
+    private void OnBecameVisible()
+    {
+        planarReflectionProbe.enabled = true;
+    }
+
+}
